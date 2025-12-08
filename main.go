a845afc02d964cf30d6d661918d8ad5ceb410fa6
Xarth-Mai <xarthleo@gmail.com>
2025-12-08 17:29:19 +0800

    refactor: relocate frontend assets

--- main.go
+++ main.go
@@ -11,25 +11,30 @@ const VERSION = "0.0.1"
 const SOCK_PATH = "/run/xarth-mai/app.sock"
 
 func main() {
+	// remove old socket
 	if err := os.RemoveAll(SOCK_PATH); err != nil {
 		fmt.Printf("Error removing old socket: %v\n", err)
 	}
 
+	// listen on socket
 	listener, err := net.Listen("unix", SOCK_PATH)
 	if err != nil {
 		fmt.Printf("Error listening on socket: %v\n", err)
 		return
 	}
+
+	// close socket when program exit
 	defer listener.Close()
 
-	frontend := http.FileServer(http.Dir("/lzzz.ink"))
+	// serve frontend
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 
-	http.HandleFunc("/", frontend.ServeHTTP)
-
+	// serve api
 	http.HandleFunc("/api/version", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "{\"version\": \"%s\"}", VERSION)
 	})
 
+	// serve and print log
 	fmt.Printf("Listening on %s\n", SOCK_PATH)
 	if err := http.Serve(listener, nil); err != nil {
 		fmt.Printf("Error serving: %v\n", err)
