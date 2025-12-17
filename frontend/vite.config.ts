3313ffacb7d9a51be790689e77905cbbde69ae88
Xarth-Mai <xarthleo@gmail.com>
2025-12-17 14:57:17 +0800

    feat: Integrate `tsgo` for type checking and `lightningcss` for CSS processing and minification, alongside build configuration updates.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,24 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(
+    {
+      compilerOptions: {
+        css: 'external',
+      }
+    }
+  )],
+  css: {
+    transformer: 'lightningcss',
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     target: 'esnext',
+    modulePreload: {
+      polyfill: false
+    },
     minify: 'oxc',
+    cssMinify: 'lightningcss',
   }
 })
